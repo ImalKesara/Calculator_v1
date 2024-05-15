b11158b3f8d115d9c100e7f4528ecb16a4a93e72
LoneWolf <imalkesara2001@gmail.com>
2024-05-15 20:55:06 +0530

    First commit

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+
+
+<div>
+  <button class="btn btn-secondary">hi</button>
+</div>
+
+
+
+
+
+
+
+
+
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
