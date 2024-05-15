dd13fd302ca2c0146cc83b15b14027ba966adc3a
LoneWolf <imalkesara2001@gmail.com>
2024-05-16 00:05:40 +0530

    done

--- src/App.svelte
+++ src/App.svelte
@@ -61,45 +61,47 @@
 </script>
 
 
+<div class="fix">
 
-<div class="container  artboard phone-1">
-  <div class="calculator">
+<div class="container  shadow-md">
+  <div class="calculator backdrop-blur-md bg-white/60">
     <div class="first-row">
       <input type="text" bind:value={console}>
     </div>
     <div class="second-row">
-      <button class="btn " on:click={()=>{setOperation('C');}}>C</button>
-      <button class="btn ">+/-</button>
-      <button class="btn ">.</button>
-      <button class="btn btn-secondary" on:click={()=>{setOperation('divide');}}> &divide</button>
+      <button class="btn glass" on:click={()=>{setOperation('C');}}>C</button>
+      <button class="btn glass">+/-</button>
+      <button class="btn glass">.</button>
+      <button class="btn btn-warning" on:click={()=>{setOperation('divide');}}> &divide</button>
     </div>
 
     <div class="third-row">
-      <button class="btn" on:click={()=>{setValue(9);}}>9</button>
-      <button class="btn" on:click={()=>{setValue(8);}}>8</button>
-      <button class="btn " on:click={()=>{setValue(7);}}>7</button>
+      <button class="btn glass" on:click={()=>{setValue(9);}}>9</button>
+      <button class="btn glass" on:click={()=>{setValue(8);}}>8</button>
+      <button class="btn glass" on:click={()=>{setValue(7);}}>7</button>
       <button class="btn btn-warning" on:click={()=>{setOperation('multiply');}}> &times; </button>
     </div>
 
     <div class="forth-row">
-      <button class="btn" on:click={()=>{setValue(6);}}>6</button>
-      <button class="btn" on:click={()=>{setValue(5);}}>5</button>
-      <button class="btn" on:click={()=>{setValue(4);}}>4</button>
+      <button class="btn glass" on:click={()=>{setValue(6);}}>6</button>
+      <button class="btn glass" on:click={()=>{setValue(5);}}>5</button>
+      <button class="btn glass" on:click={()=>{setValue(4);}}>4</button>
       <button class="btn btn-warning" on:click={()=>{setOperation('add');}}> + </button>
     </div>
 
     <div class="fifth-row">
-      <button class="btn" on:click={()=>{setValue(3);}}>3</button>
-      <button class="btn" on:click={()=>{setValue(2);}}>2</button>
-      <button class="btn" on:click={()=>{setValue(1);}}>1</button>
+      <button class="btn glass" on:click={()=>{setValue(3);}}>3</button>
+      <button class="btn glass" on:click={()=>{setValue(2);}}>2</button>
+      <button class="btn glass" on:click={()=>{setValue(1);}}>1</button>
       <button class="btn btn-warning" on:click={()=>{setOperation('substract');}}> - </button>
     </div>
 
     <div class="sixth-row">
-      <button class="btn " on:click={()=>{setValue(0);}}>0</button>
+      <button class="btn glass" on:click={()=>{setValue(0);}}>0</button>
       <button class="btn btn-error" on:click={()=>{equal();}}>=</button>
     </div>
   </div>
+</div>
 </div>
 
 
@@ -110,21 +112,58 @@
   @tailwind utilities;
 
 
- 
+.fix{
+  background: rgb(247,41,255);
+ background: radial-gradient(circle, rgba(247,41,255,1) 0%, rgba(223,76,250,1) 35%, rgba(148,187,233,1) 100%); 
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+} 
 
 .container {
-  max-width: 350px;
+  
+  max-width: 380px;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
 }
 
+.sixth-row button:first-child {
+  display: grid;
+  width: 200px; /* Half the original width */
+  height: 60px;
+  font-size: 20px;
+  border-radius: 10px;
+  color: white;
+}
 
+.sixth-row button:last-child {
+  display: grid;
+  width: 100px; /* Half the original width */
+  height: 60px;
+  font-size: 20px;
+  border-radius: 10px;
+  color: white;
+}
+
+
+
+.fifth-row button,.forth-row button,.third-row button,.second-row button{
+  display: grid;
+  width: 60px;
+  height:60px;
+  outline: none;
+  font-size: 20px;
+  border-radius: 10px;
+  color: white;
+}
 
 .calculator {
-  background-color: white;
-  padding: 20px 10px;
+  padding: 20px ;
   display: grid;
   grid-template-columns: repeat(6, 1fr);
   gap: 18px;
@@ -137,6 +176,7 @@
 }
 
 .first-row input {
+  background-color: white;
   outline: none;
   padding: 15px;
   width: 100%;
@@ -152,6 +192,8 @@
   display: grid;
   grid-column: 1 / 7;
   grid-auto-flow: column;
+  justify-items: center;
+  align-items: center;
   gap: 10px;
 }
 
