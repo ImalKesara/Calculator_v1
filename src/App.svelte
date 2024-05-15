532eea2b53082d6f4fda8f45f6c3c43069879926
LoneWolf <imalkesara2001@gmail.com>
2024-05-15 21:58:19 +0530

    main ui

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,7 @@
 <script>
-	import Calculator from './components/Calculator.svelte';
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
-
+  let total =0;
+  let console = 0;
+  let state =null;
   function resolveState(){
 
   }
@@ -19,15 +17,47 @@
 
 </script>
 
-<Calculator />
 
 
+<div class="container">
+  <div class="calculator">
+    <div class="first-row">
+      <input type="text" bind:value={console}>
+    </div>
+    <div class="second-row">
+      <button class="btn btn-ghost">AC</button>
+      <button class="btn btn-ghost">+/-</button>
+      <button class="btn btn-ghost">.</button>
+      <button class="btn btn-warning">/</button>
+    </div>
 
-<div>
-  <button class="btn btn-secondary">hi</button>
+    <div class="third-row">
+      <button class="btn">9</button>
+      <button class="btn">8</button>
+      <button class="btn">7</button>
+      <button class="btn btn-warning"></button>
+    </div>
+
+    <div class="forth-row">
+      <button class="btn">6</button>
+      <button class="btn">5</button>
+      <button class="btn">4</button>
+      <button class="btn btn-warning"></button>
+    </div>
+
+    <div class="fifth-row">
+      <button class="btn">3</button>
+      <button class="btn">2</button>
+      <button class="btn">1</button>
+      <button class="btn btn-warning"></button>
+    </div>
+
+    <div class="sixth-row">
+      <button class="btn">0</button>
+      <button class="btn btn-warning">=</button>
+    </div>
+  </div>
 </div>
-
-
 
 
 
@@ -40,4 +70,46 @@
   @tailwind base;
   @tailwind components;
   @tailwind utilities;
+
+  
+
+  .container{
+
+    max-width: 350px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+   
+  }
+
+  .calculator{
+    background-color: white;
+    padding: 15px;
+    display: grid;
+    grid-template-columns: repeat(6, 1fr);
+    gap: 10px;
+  }
+
+  .first-row {
+    grid-column: 1 / 7;
+    grid-row: 1;
+
+  }
+
+  .first-row input{
+    padding: 10px;
+    width: 100%;
+    height: 50px;
+  }
+
+  .second-row,.third-row,.forth-row,.fifth-row,.sixth-row{
+    display: grid;
+    grid-column: 1 / 7;
+    grid-auto-flow: column;
+    gap: 10px;
+  }
+  
+
+
 </style>
