d6447f791f61e5547abde2fe663c10ac51d9ec1d
LoneWolf <imalkesara2001@gmail.com>
2024-05-15 22:38:05 +0530

    functional part is done

--- src/App.svelte
+++ src/App.svelte
@@ -1,18 +1,61 @@
 <script>
-  let total =0;
-  let console = 0;
+  let total = 0;
+  let console = "";
   let state =null;
   function resolveState(){
+    switch(state){
+      case 'add':
+        total += parseFloat(console);
+        console = "0";
+        break;
+
+      case 'substract':
+      total -= parseFloat(console);
+      console = "0";
+      break;
+
+      case 'multiply':
+      total *= parseFloat(console);
+      console = "0";
+      break;
+
+      case 'divide':
+      total /= parseFloat(console);
+      console = "0";
+      break;
+
+      default:
+      total = parseFloat(console);
+      console = "0";
+      break;
+
+
+    }
 
   }
   function setOperation(operation){
-
+    resolveState();
+    state = operation;
   }
   function setValue(value){
+    if(console.toString() == "0" || state == "equal"){
+      console = "";
+    }
+
+    if(value == "C"){
+      total =0;
+      state = null;
+      console = ""
+      return
+    }
+
+    console = console + value;
 
   }
   function equal(){
-
+    resolveState()
+    console = total.toString();
+    state = "equal"
   }
 
 </script>
@@ -25,36 +68,36 @@
       <input type="text" bind:value={console}>
     </div>
     <div class="second-row">
-      <button class="btn btn-ghost">AC</button>
-      <button class="btn btn-ghost">+/-</button>
-      <button class="btn btn-ghost">.</button>
-      <button class="btn btn-warning">/</button>
+      <button class="btn " on:click={()=>{setOperation('C');}}>C</button>
+      <button class="btn ">+/-</button>
+      <button class="btn ">.</button>
+      <button class="btn btn-warning" on:click={()=>{setOperation('divide');}}> &divide</button>
     </div>
 
     <div class="third-row">
-      <button class="btn">9</button>
-      <button class="btn">8</button>
-      <button class="btn">7</button>
-      <button class="btn btn-warning"></button>
+      <button class="btn" on:click={()=>{setValue(9);}}>9</button>
+      <button class="btn" on:click={()=>{setValue(8);}}>8</button>
+      <button class="btn" on:click={()=>{setValue(7);}}>7</button>
+      <button class="btn btn-warning" on:click={()=>{setOperation('multiply');}}> &times; </button>
     </div>
 
     <div class="forth-row">
-      <button class="btn">6</button>
-      <button class="btn">5</button>
-      <button class="btn">4</button>
-      <button class="btn btn-warning"></button>
+      <button class="btn" on:click={()=>{setValue(6);}}>6</button>
+      <button class="btn" on:click={()=>{setValue(5);}}>5</button>
+      <button class="btn" on:click={()=>{setValue(4);}}>4</button>
+      <button class="btn btn-warning" on:click={()=>{setOperation('add');}}> + </button>
     </div>
 
     <div class="fifth-row">
-      <button class="btn">3</button>
-      <button class="btn">2</button>
-      <button class="btn">1</button>
-      <button class="btn btn-warning"></button>
+      <button class="btn" on:click={()=>{setValue(3);}}>3</button>
+      <button class="btn" on:click={()=>{setValue(2);}}>2</button>
+      <button class="btn" on:click={()=>{setValue(1);}}>1</button>
+      <button class="btn btn-warning" on:click={()=>{setOperation('substract');}}> - </button>
     </div>
 
     <div class="sixth-row">
-      <button class="btn">0</button>
-      <button class="btn btn-warning">=</button>
+      <button class="btn" on:click={()=>{setValue(0);}}>0</button>
+      <button class="btn btn-error" on:click={()=>{equal();}}>=</button>
     </div>
   </div>
 </div>
@@ -98,7 +141,8 @@
   }
 
   .first-row input{
-    padding: 10px;
+    outline: none;
+    padding: 15px;
     width: 100%;
     height: 50px;
   }
